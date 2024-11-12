80ece41cbe732331ccc32e040984c8b98b540c02
abdullah-works <abdullah.worke@gmail.com>
2024-11-12 20:48:55 +0500

    configured basic scaffold & appBar

--- a_dicee_app/lib/main.dart
+++ a_dicee_app/lib/main.dart
@@ -10,6 +10,31 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: const Center(
+            child: Text('Dicee App'),
+          ),
+          backgroundColor: const Color.fromARGB(255, 42, 42, 42),
+        ),
+        body: const DicePage(),
+      ),
+    );
+  }
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
